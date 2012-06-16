0418a72d290f445adc6804ae6677989bb4094572
DOP <gmdopp@gmail.com>
2012-06-16 15:14:55 +0400

    include/HMM.h: defined HiddenMarkovModels namespace

--- include/HMM.h
+++ include/HMM.h
@@ -16,73 +16,76 @@
 namespace ublas = boost::numeric::ublas; // namespace alias (saner than 'using ...')
 using namespace boost::assign;
 
-typedef double real_type;
-typedef std::vector<unsigned int> sequence_type;
 
-std::ostream &operator<<(std::ostream &cout, sequence_type s);
-
-ublas::vector<real_type> vlog(const ublas::vector<real_type> &u);
-
-ublas::matrix<real_type> mlog(const ublas::matrix<real_type> &A);
-
-class HMM
+namespace HiddenMarkovModels
 {
-private:
-  int _nstates;
-  int _nsymbols;
-  ublas::matrix<real_type> _transition;
-  ublas::matrix<real_type> _emission;
-  ublas::vector<real_type> _pi;
+  typedef double real_type;
+  typedef std::vector<unsigned int> sequence_type;
 
-public:
-  HMM(ublas::matrix<real_type> transition,
-      ublas::matrix<real_type> emission,
-      ublas::vector<real_type> pi);
+  std::ostream &operator<<(std::ostream &cout, sequence_type s);
 
-  int get_nstates(void);
+  ublas::vector<real_type> vlog(const ublas::vector<real_type> &u);
+ 
+  ublas::matrix<real_type> mlog(const ublas::matrix<real_type> &A);
 
-  int get_nsymbols(void);
-
-  const ublas::matrix<real_type> &get_transition(void);
-
-  const ublas::matrix<real_type> &get_emission(void);
-
-  const ublas::vector<real_type> &get_pi(void);
-
-  bool isSymbol(unsigned int i);
-
-  boost::tuple<sequence_type, // optimal path
-	       real_type // likelihood of path
-	       > viterbi(const sequence_type &obseq);
-
-  boost::tuple<ublas::matrix<real_type>, // alpha-hat
-	       ublas::matrix<real_type>, // beta-hat
-	       ublas::matrix<real_type>, // gamma-hat
-	       boost::multi_array<real_type,3>, // epsilon-hat
-	       real_type // likelihood
-	       > forward_backward(const sequence_type &obseq);
-
-  boost::tuple<HMM, // the learned model
-	       real_type // the likelihood of the sequences of observations, under this new model
-	       > learn(const std::vector<sequence_type> &obseqs);
-
-  boost::tuple<HMM, // the learned model
-	       real_type // the likelihood of the sequences of observations, under this new model
-	       > baum_welch(const std::vector<sequence_type> &obseqs, //
-			    real_type tolerance=1e-9, // tolerance level for convergence
+  class HMM
+  {
+  private:
+    int _nstates;
+    int _nsymbols;
+    ublas::matrix<real_type> _transition;
+    ublas::matrix<real_type> _emission;
+    ublas::vector<real_type> _pi;
+    
+  public:
+    HMM(ublas::matrix<real_type> transition,
+	ublas::matrix<real_type> emission,
+	ublas::vector<real_type> pi);
+    
+    int get_nstates(void);
+    
+    int get_nsymbols(void);
+    
+    const ublas::matrix<real_type> &get_transition(void);
+    
+    const ublas::matrix<real_type> &get_emission(void);
+    
+    const ublas::vector<real_type> &get_pi(void);
+    
+    bool isSymbol(unsigned int i);
+    
+    boost::tuple<sequence_type, // optimal path
+      real_type // likelihood of path
+      > viterbi(const sequence_type &obseq);
+    
+    boost::tuple<ublas::matrix<real_type>, // alpha-hat
+      ublas::matrix<real_type>, // beta-hat
+      ublas::matrix<real_type>, // gamma-hat
+      boost::multi_array<real_type,3>, // epsilon-hat
+      real_type // likelihood
+      > forward_backward(const sequence_type &obseq);
+    
+    boost::tuple<HMM, // the learned model
+      real_type // the likelihood of the sequences of observations, under this new model
+      > learn(const std::vector<sequence_type> &obseqs);
+    
+    boost::tuple<HMM, // the learned model
+      real_type // the likelihood of the sequences of observations, under this new model
+      > baum_welch(const std::vector<sequence_type> &obseqs, //
+		   real_type tolerance=1e-9, // tolerance level for convergence
 			    unsigned int maxiter=200 // maximum number of iterations
-			    );
-};
-
-boost::tuple<int,
-	     real_type
-	       > argmax(ublas::vector<real_type> &u);
-
-std::ostream &operator<<(std::ostream &cout, HMM &hmm);
-
-ublas::matrix<real_type> load_hmm_matrix(const char *filename);
-
-ublas::vector<real_type> load_hmm_vector(const char *filename);
-
-std::vector<sequence_type> load_hmm_observations(const char *filename);
-
+		   );
+  };
+  
+  boost::tuple<int,
+    real_type
+    > argmax(ublas::vector<real_type> &u);
+  
+  std::ostream &operator<<(std::ostream &cout, HMM &hmm);
+  
+  ublas::matrix<real_type> load_hmm_matrix(const char *filename);
+  
+  ublas::vector<real_type> load_hmm_vector(const char *filename);
+  
+  std::vector<sequence_type> load_hmm_observations(const char *filename);
+}
