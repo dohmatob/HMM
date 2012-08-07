6b4d81bb8d5098a40b166e1ca5f18bc103c7586d
DOP (dohmatob elvis dopgima) <gmdopp@gmail.com>
2012-08-07 22:28:12 +0200

    spike/HMM/python/entropy_map.py: re-implemented from scratch

--- spike/HMM/python/entropy_map.py
+++ spike/HMM/python/entropy_map.py
@@ -1,72 +1,103 @@
 # (c) DOP (dohmato elvis dopgima)
+#
+# Inspired by Matthiew Brand's "Pattern learning via entropy maximization"
 
 import scipy.special.lambertw as W
 from scipy import e, isreal
-from numpy import sum, log, exp, abs, array, min, max
+from numpy import sum, log, exp, abs, min, max, inf, nonzero, all, array, real, mean
+
+# constants
+_BEAR = -50 # exp(-x) is near 0 for x > _BEAR
 
 def normalize_probabilities(x):
+    """
+    Function to normalize a probability vector so it becomes stochasitc
+    """
     return x/sum(x, dtype='float64')
 
 def entropic_estimate(omega, 
                       theta, 
-                      nu=-1, 
-                      _lambda=None, 
+                      Z=1, # relative importance of entropy over likelihood
                       maxiter=100, 
-                      tol=1e-7):
+                      tol=1e-7,
+                      ):
     """
     Re-estimates a parameter (statistic) theta = (theta[0], theta[1], ...) based on evidence vector omega.
-    omega should contain no zero evidence and theta should have no zero coordinates (if these conditions fail,
+    XXX omega should contain no zero evidence and theta should have no zero coordinates (if these conditions fail,
     then simply trim off the offending parameters!)
     """
+    
+    assert Z != 0
 
-    # routine sanity checks
-    assert len(omega) == len(theta)
-    assert nu < 0
+    # all arrays must be numpy-like
+    omega = array(omega)
+    theta = array(theta)
 
-    # prepare initial _lambda value which will maintain the Lambert function on the lower real branch W_-1
-    default_lambda = (2/nu+log(-nu*max(omega/theta))/nu) - 1 # or anythx less than (2/nu+log(-nu*max(omega/theta))/nu)
-    if _lambda is None:
-        _lambda = default_lambda
-    print _lambda, default_lambda
-    _lambda = min([_lambda, default_lambda])
-        
+    # prepare initial _lambda which will ensure that Lambert's W is real-valued
+    if Z > 0:
+        critical_lambda = min(-Z*(2 + log(omega/Z)))
+        _lambda = critical_lambda - 1 # or anything less than the critical value above
+    elif Z < 0:
+        _lambda = -mean(Z*(log(theta) + 1) + omega/theta)
+    assert all(-omega*exp(1+_lambda/Z)/Z > -1/e), -omega*exp(1+_lambda/Z)/Z 
+    
     # Fixed-point loop
-    L = len(omega)
     theta_hat = theta
     iteration = 0
-    while True:
+    relative_gain = inf
+    print "entropy_map: starting Fixed-point loop .."
+    print
+    while relative_gain >= tol:
         # exhausted ?
         if maxiter < iteration:
-            print "entropic_estimate: did not converge after %d iterations (tolerance was set to %s)"%(maxiter,tol)
             break
 
-        # re-estimate theta parameter vector
         print "Iteration: %d"%iteration
-        print "\tlambda: %s"%_lambda
-        for i in xrange(L):
-            z = nu*omega[i]*exp(1-nu*_lambda)/theta[i]
-            assert -1 <= e*z < 0, "Lambert's W: arg %s out of range"%z
-            g = W(z, k=-1) # lower-branch of W on -1/e <= z < 0 verifying W <= -1, denoted W_-1
-            assert isreal(g)
+        print 'Current parameter estimate:\n%s'%theta
+        print 'lambda:', _lambda
+        print "Relative gain in lambda over last iteration: %s"%relative_gain
 
-            # compute theta_hat[i] given _lambda
-            theta_hat[i] = nu*omega[i]/g 
+        # if necessary, re-scale learning rate (Z) so that exp(1 + _lambda/Z) is not 'too small'
+        if _lambda < 0:
+            if Z > 0:
+                new_Z = max([Z, -_lambda/_BEAR])
+            elif Z < 0:
+                new_Z = min([Z, -_lambda/_BEAR])
+            if Z != new_Z:
+                Z = new_Z
+                print "N.B: Re-scaled learning rate (Z) to %s so Lambert's W function doesn't vanish."%Z
+
+        # prepare argument for Lambert's W function
+        z = -omega*exp(1 + _lambda/Z)/Z
+        assert all(isreal(z)) 
+        if any(z < -1/e):
+            print "Lambert's W: arg %s out of range"%z
+            break
+
+        # compute Lambert's W function at z
+        if Z <= 0:
+            g = W(z, k=0)
+        else:
+            g = W(z, k=-1)
+        assert all(isreal(g))
+        g = real(g)
+        
+        # check against division by zero
+        if any(g == 0):
+            print 'Fatal!'
+            break
+
+        # re-estimate theta
+        theta_hat = (-omega/Z)/g 
 
         # normalize the approximated theta_hat parameter
         theta_hat = normalize_probabilities(theta_hat)
 
         # re-estimate _lambda
-        _lambda_hat = -omega[0]/theta_hat[0] + log(theta_hat[0]/theta[0])/nu + 1/nu
+        _lambda_hat = -(Z*(log(theta_hat[0]) + 1) + omega[0]/theta_hat[0])
 
         # compute relative gain in _lambda
         relative_gain = abs((_lambda - _lambda_hat)/_lambda)        
-        print "\tRelative gain in lambda over last iteration: %s"%relative_gain
-        print
-
-        # converged ?
-        if relative_gain < tol:
-            print "entropic_estimate: converged after %d iterations (tolerance was set to %s)"%(iteration,tol)
-            break
 
         # update _lambda and theta
         _lambda = _lambda_hat
@@ -74,9 +105,29 @@ def entropic_estimate(omega,
 
         # goto next iteration
         iteration += 1
-    
-    return theta_hat
+
+        print
+
+    print "Done."
+
+    # converged ?
+    if relative_gain < tol:
+        print "entropic_estimate: loop converged after %d iterations (tolerance was set to %s)"%(iteration,tol)
+    else:
+        print "entropic_estimate: loop did not converge after %d iterations (tolerance was set to %s)"\
+            %(maxiter,tol)
+
+    print "Final learning rate (Z):", Z    
+    print "Final parameters:\n%s"%theta_hat
+
+    # render results
+    return theta_hat, Z, _lambda
 
 if __name__ == '__main__':
-    print entropic_estimate(array([1,2,3]), array([0.01,0.67,0.32]))
+    from hmm import almost_uniform_vector
+    
+    omega = [1, 2]
+    theta = almost_uniform_vector(len(omega))
+    theta_hat = entropic_estimate(omega, theta, Z=1)
+
     
